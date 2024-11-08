44528e09102dbada1501434d23f6d23c86b23a08
accomplied24 <can_ulak1972@hotmail.com>
2024-11-08 17:48:55 +0300

    initial project add. Closes #1

---
+++ src/main/java/com/toker/sodaservice/SodaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.toker.sodaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toker/sodaservice/SodaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toker.sodaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
